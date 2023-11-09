1b16610c7a946f38c0ad5308a98db622602d53e3
Son Andreea <82153848+andreea-son@users.noreply.github.com>
2023-11-09 19:31:58 +0200

    Add files via upload

---
+++ lab5/pb1.py
@@ -0,0 +1,7 @@
+# 1) 
+# a) semnalul este esantionat din ora in ora => freq = 1/3600 Hz
+# b) fiind 18288 esantionane din ora in ora, intervalul
+# de timp acoperit este de 18288 h
+# c) frecventa maxima prezenta in semnal este de 1/1800 = 0.000(5) Hz
+
+# d) TODO
